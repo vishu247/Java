d617eadf379d98f97c25407f2d1eed94c3a6543c
vishugoyal <vishu.goyal@devtron.ai>
2024-05-27 11:08:04 +0530

    added the task

--- src/main/java/com/joesalt/tutorial/TutorialApplication.java
+++ src/main/java/com/joesalt/tutorial/TutorialApplication.java
@@ -2,8 +2,11 @@ package com.joesalt.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+// @ComponentScan(basePackages = "com.joesalt.tutorial")
 public class TutorialApplication {
 
 	public static void main(String[] args) {
